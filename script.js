93a7da96bf320399709e7b1a6e7f1ab660b89749
Rafael Ramalho <160792709+rafael-ramalho@users.noreply.github.com>
2024-06-25 14:01:32 -0300

    Script da calculadora adicionado

--- script.js
+++ script.js
@@ -1,31 +1,100 @@
 function add(a, b){
-    return a + b;
+    return Math.round((a + b) * 100000) / 100000;
 };
 
 function subtract(a, b){
-    return a - b;
+    return Math.round((a - b) * 100000) / 100000;
 };
 
 function multiply(a, b){
-    return a * b;
+    return Math.round((a * b) * 100000) / 100000;
 };
 
 function divide(a, b){
-    return a / b;
+    if(b === 0) return 'ERROR';
+    return Math.round((a / b) * 100000) / 100000;
 };
 
-let firstNumber
-let secondNumber
-let operator
-
 function operate(operator, firstNumber, secondNumber){
-    if (operator === '+') return add(firstNumber, secondNumber);
-    if (operator === '-') return subtract(firstNumber, secondNumber);
-    if (operator === '*') return multiply(firstNumber, secondNumber);
-    if (operator === '/') return divide(firstNumber, secondNumber);
+    let num1 = Number(firstNumber);
+    let num2 = Number(secondNumber);
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === '*') return multiply(num1, num2);
+    if (operator === '/') return divide(num1, num2);
 }
 
-let displayValue = document.querySelector('#display');
-displayValue.addEventListener('click', () => {
-    
-})
+function calculate(calculation){
+    let array = calculation.split(' ');
+    return operate(array[1], array[0], array[2]);
+}
+
+let operator;
+let firstNumber;
+let secondNumber;
+let operatorTyped; // checa se já escreveu um operador antes
+let secondOperation; // operações subsequentes
+let decimalTyped = false;
+
+let display = document.querySelector('#display');
+
+let btn = document.querySelectorAll('button');
+
+btn.forEach(item => item.addEventListener('click', () => {
+    const op = ['+', '-', '*', '/'];
+    const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+
+    if(nums.includes(item.textContent)){
+        if(item.textContent == '.' && display.textContent == '') {
+            display.textContent = '';
+        } else if (secondOperation === true || op.includes(display.textContent)) {
+            display.textContent = item.textContent;
+            secondOperation = false;
+        } else if (item.textContent == '.' && decimalTyped === false) {
+            display.textContent += item.textContent;
+            decimalTyped = true;
+        } else if (item.textContent != '.') {
+            display.textContent += item.textContent;
+        };
+    };
+    if(op.includes(item.textContent)){
+        if (op.includes(display.textContent)) {
+            display.textContent = item.textContent;
+            operator = item.textContent;
+        } else if (nums.includes(display.textContent.charAt(0)) && operatorTyped === true) {
+            secondNumber = display.textContent;
+            display.textContent = operate(operator, firstNumber, secondNumber);
+            firstNumber = display.textContent;
+            operator = item.textContent;
+            secondOperation = true;
+            decimalTyped = false;
+        } else {
+            firstNumber = display.textContent;
+            display.textContent = item.textContent;
+            operator = item.textContent;
+            operatorTyped = true;
+            decimalTyped = false;
+        };
+    };
+    if(item.textContent == '=') {
+        if (nums.includes(display.textContent.charAt(display.textContent.length-1))){
+            secondNumber = display.textContent;
+            display.textContent = operate(operator, firstNumber, secondNumber);
+            operator = undefined;
+            firstNumber = undefined;
+            secondNumber = undefined;
+            operatorTyped = false;
+            secondOperation = false;
+            decimalTyped = false;
+        };
+    };
+    if(item.textContent == 'C') {
+        display.textContent = '';
+        operator = undefined;
+        firstNumber = undefined;
+        secondNumber = undefined;
+        operatorTyped = false;
+        secondOperation = false;
+        decimalTyped = false;
+    }
+}));
